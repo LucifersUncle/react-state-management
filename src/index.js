483df8d269b53e4a6d2903f126fc4bcb475fd233
LucifersUncle <201801604@post.au.dk>
2023-01-09 07:50:17 +0100

    added the service worker

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
+
+//should register the app as a PWA 
+serviceWorkerRegistration.register();
